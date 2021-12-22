0c9f3e6088f0e1ef4d88ce1d25d23c0c83a05528
ayudewita <dewita008@gmail.com>
2021-12-22 15:36:37 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { StoreProvider } from 'easy-peasy'
+import store from './store'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    <Router> 
+      <StoreProvider store={store}>        
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>         
+      </StoreProvider>         
+    </Router>   
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
